27aa6a5a10b7e187a175cd6584041034f2013a5c
Paulo Henrique Capuzzo <paulocapucci@gmail.com>
2023-08-12 22:49:22 -0300

    Perfil de teste criado

--- src/main/java/com/valdir/helpesk/HelpdeskApplication.java
+++ src/main/java/com/valdir/helpesk/HelpdeskApplication.java
@@ -1,48 +1,13 @@
 package com.valdir.helpesk;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.valdir.helpesk.domain.Chamado;
-import com.valdir.helpesk.domain.Cliente;
-import com.valdir.helpesk.domain.Tecnico;
-import com.valdir.helpesk.domain.enums.Perfil;
-import com.valdir.helpesk.domain.enums.Prioridade;
-import com.valdir.helpesk.domain.enums.Status;
-import com.valdir.helpesk.repositories.ChamadoRepository;
-import com.valdir.helpesk.repositories.ClienteRepository;
-import com.valdir.helpesk.repositories.TecnicoRepository;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
-
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private ChamadoRepository chamadoRepository;
+public class HelpdeskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Tecnico tec1 = new Tecnico(null, "Paulo Capuzzo", "29740434886", "paulocapucci@gmail.com", "123");
-		tec1.addPerfil(Perfil.ADMIN);
-
-		Cliente cli1 = new Cliente(null, "Steve Jobs", "12312312398", "steve@gmail.com", "123");
-
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.MEDIA, "Chamado 01", "Primeiro chamado", tec1, cli1);
-
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
 
 }
---
+++ src/main/java/com/valdir/helpesk/services/DBService.java
@@ -0,0 +1,40 @@
+package com.valdir.helpesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.valdir.helpesk.domain.Chamado;
+import com.valdir.helpesk.domain.Cliente;
+import com.valdir.helpesk.domain.Tecnico;
+import com.valdir.helpesk.domain.enums.Perfil;
+import com.valdir.helpesk.domain.enums.Prioridade;
+import com.valdir.helpesk.domain.enums.Status;
+import com.valdir.helpesk.repositories.ChamadoRepository;
+import com.valdir.helpesk.repositories.ClienteRepository;
+import com.valdir.helpesk.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+
+	public void instanciaDB() {
+		Tecnico tec1 = new Tecnico(null, "Paulo Capuzzo", "29740434886", "paulocapucci@gmail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+
+		Cliente cli1 = new Cliente(null, "Steve Jobs", "12312312398", "steve@gmail.com", "123");
+
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.MEDIA, "Chamado 01", "Primeiro chamado", tec1, cli1);
+
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+}
