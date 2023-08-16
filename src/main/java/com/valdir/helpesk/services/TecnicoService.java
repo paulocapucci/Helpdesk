a26088f9ebef00357af616139f319a30472dba4f
Paulo Henrique Capuzzo <paulocapucci@gmail.com>
2023-08-16 20:11:38 -0300

    exceção para objeto não encontrado

--- src/main/java/com/valdir/helpesk/services/TecnicoService.java
+++ src/main/java/com/valdir/helpesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.valdir.helpesk.domain.Tecnico;
 import com.valdir.helpesk.repositories.TecnicoRepository;
+import com.valdir.helpesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
 	}
 }
